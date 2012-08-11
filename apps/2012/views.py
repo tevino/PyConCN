11fb27622feb83cada1cb593e2fe88e979d466e6
gfreezy <gfreezy@gmail.com>
2012-08-11 14:44:19 +0800

    views for 2012

---
+++ apps/2012/views.py
@@ -0,0 +1,29 @@
+#coding=utf-8
+from uliweb import expose
+
+
+@expose('/2012/')
+def index():
+    return {}
+
+
+@expose('/2012')
+class siteView(object):
+
+    def about(self):
+        return dict(page=dict(pagename='about', cndata=''))
+
+    def schedule(self):
+        return dict(page=dict(pagename='schedule', cndata=''))
+
+    def collections(self):
+        return dict(page=dict(pagename='collections', cndata=''))
+
+    def registration(self):
+        return dict(page=dict(pagename='registration', cndata=''))
+
+    def sponsors(self):
+        return dict(page=dict(pagename='sponsors', cndata=''))
+
+    def volunteer(self):
+        return dict(page=dict(pagename='volunteer', cndata=''))
