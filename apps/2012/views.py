9eafb35a16eb53ab73447e780b04b86e542a1c78
Zoom.Quiet <zoomquiet+git@gmail.com>
2012-09-02 12:17:23 +0800

    appended title for all /2012 views

--- apps/2012/views.py
+++ apps/2012/views.py
@@ -1,30 +1,29 @@
 #coding=utf-8
 from uliweb import expose
 
-
+pagetitle = "PyCon China 2012"
 @expose('/2012')
 class SiteView2012(object):
     @expose('')
     def index(self):
-        return {"year":2012, "title":"PyCon China 2012"}
+        return {"year":2012, "title":pagetitle}
     
     def about(self):
-        return dict(page=dict(pagename='about', cndata=''))
+        return dict(page=dict(pagename='about', cndata=''),title=pagetitle)
 
     def schedulebj(self):
-        return dict(page=dict(pagename='schedulebj', cndata=''))
+        return dict(page=dict(pagename='schedulebj', cndata=''),title=pagetitle)
 
     def schedulesh(self):
-        return dict(page=dict(pagename='schedulesh', cndata=''))
+        return dict(page=dict(pagename='schedulesh', cndata=''),title=pagetitle)
 
     def collections(self):
-        return dict(page=dict(pagename='collections', cndata=''))
-
+        return dict(page=dict(pagename='collections', cndata=''),title=pagetitle)
     def registration(self):
-        return dict(page=dict(pagename='registration', cndata=''))
+        return dict(page=dict(pagename='registration', cndata=''),title=pagetitle)
 
     def sponsors(self):
-        return dict(page=dict(pagename='sponsors', cndata=''))
+        return dict(page=dict(pagename='sponsors', cndata=''),title=pagetitle)
 
     def volunteer(self):
-        return dict(page=dict(pagename='volunteer', cndata=''))
+        return dict(page=dict(pagename='volunteer', cndata=''),title=pagetitle)
